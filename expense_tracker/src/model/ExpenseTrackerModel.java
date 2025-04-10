32f15687c5a0b78bf0ba23043d0ba66207eb8687
subhangi2731 <subhangichoudhary6@gmail.com>
2025-04-10 05:32:33 -0400

    Encapsulate transactions list and return immutable copy to prevent immutability

--- expense_tracker/src/model/ExpenseTrackerModel.java
+++ expense_tracker/src/model/ExpenseTrackerModel.java
@@ -1,26 +1,56 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
+
+/**
+* The ExpenseTrackerModel class manages a list of transactions.
+* It ensures encapsulation and immutability by restricting direct access
+* and returning unmodifiable views of internal data.
+*/
 public class ExpenseTrackerModel {
 
-  public List<Transaction> transactions;
 
-  public ExpenseTrackerModel() {
-    transactions = new ArrayList<>(); 
-  }
+   // Encapsulated list to store all transactions
+   private final List<Transaction> transactionList;
 
-  public void addTransaction(Transaction t) {
-    transactions.add(t);
-  }
 
-  public void removeTransaction(Transaction t) {
-    transactions.remove(t);
-  }
+   public ExpenseTrackerModel() {
+       this.transactionList = new ArrayList<>();
+   }
 
-  public List<Transaction> getTransactions() {
-    return transactions;
-  }
 
+   /**
+    * Adds a new transaction to the list.
+    *
+    * @param transaction the transaction to be added
+    */
+   public void addTransaction(Transaction transaction) {
+       transactionList.add(transaction);
+   }
+
+
+   /**
+    * Removes a transaction from the list.
+    *
+    * @param transaction the transaction to be removed
+    */
+   public void removeTransaction(Transaction transaction) {
+       transactionList.remove(transaction);
+   }
+
+
+   /**
+    * Returns an unmodifiable copy of the transaction list to preserve immutability.
+    *
+    * - Uses Collections.unmodifiableList(...) to explicitly make the returned list read-only.
+    * - Uses constructor shorthand new ArrayList<>(transactionList) to make a shallow copy.
+    *
+    * @return a read-only list of transactions
+    */
+   public List<Transaction> getTransactions() {
+       return Collections.unmodifiableList(new ArrayList<>(transactionList));
+   }
 }
